70c661175c66754c01de88e6930ca6dba6df3d31
Arye Eidelman <aryedov.e@gmail.com>
2019-08-02 11:29:00 -0400

    Done.

--- src/components/ControlledInput.js
+++ src/components/ControlledInput.js
@@ -1,1 +1,36 @@
-// Code ControlledInput Component Here
+import React from 'react'
+
+export default class ControlledInput extends React.Component {
+
+	constructor(props) {
+		super(props)
+		this.state = {
+			fields: {
+				...props.fields
+			}
+		}
+	}
+
+	handleFieldChange = e => {
+		this.setState({
+			...this.state,
+			fields: {
+				...this.state.fields,
+				[e.target.name]: e.target.value,
+			}
+		}, () => console.log(this.state))
+	}
+
+	render() {
+		return (
+			<form>
+				{Object.keys(this.state.fields).map(fieldName =>
+					<div>
+						<label type="text" htmlFor={fieldName}> {fieldName} </label>:
+						<input type="text" id={fieldName} value={this.state.fields[fieldName]} onChange={this.handleFieldChange} />
+					</div>
+				)}
+			</form>
+		)
+	}
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ControlledInput from './components/ControlledInput.js';
 
 ReactDOM.render(
-  <div>Mount Components Here</div>,
-  document.getElementById('root')
+	<div>
+		<ControlledInput fields={{ "First Name": "", "Last Name": "", "Favorite Programing Language": "Typescript" }}/>
+	</div>,
+	document.getElementById('root')
 );
